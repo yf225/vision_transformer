a067c0e76dd714785daacf29c648552e6a0fb76d
Will Feng (DPER) <willfeng@fb.com>
2021-11-22 23:56:01 -0800

    update

--- vit_jax/train_vit_dummy_data.py
+++ vit_jax/train_vit_dummy_data.py
@@ -1,10 +1,12 @@
 # On Cloud VM, run
 """
 pip install --upgrade pip
+export PATH=/home/yfeng_us/.local/bin:${PATH}
 pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 # NOTE: use `python3` executable!!!!!
 
+sudo pip uninstall six typing-extensions
 pip install flax
 pip install einops
 pip install tensorflow_datasets
@@ -12,7 +14,8 @@ pip install tensorflow_datasets
 # Clone repository and pull latest changes.
 rm -rf vision_transformer || true
 git clone --depth=1 https://github.com/yf225/vision_transformer -b vit_dummy_data
-cd vision_transformer && git pull
+
+python3 vision_transformer/train_vit_dummy_data.py
 """
 
 # References:
