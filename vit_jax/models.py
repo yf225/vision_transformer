b3bec0ac1e5d6889ba9f8910a7b844b898fc0328
Will Feng (DPER) <willfeng@fb.com>
2021-12-03 22:19:51 -0800

    update

--- vit_jax/models.py
+++ vit_jax/models.py
@@ -195,6 +195,7 @@ class VisionTransformer(nn.Module):
     x = inputs
 
     # Transformer.
+    print("x.shape: ", x.shape)
     n, h, w, c = x.shape
     x = jnp.reshape(x, [n, (h // self.patch_size) * (w // self.patch_size), self.patch_size * self.patch_size * c])
 
